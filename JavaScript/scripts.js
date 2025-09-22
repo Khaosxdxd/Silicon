26019784de85508461ae1cbe36338b42124cd35c
Mikel <mickis2010.mj@gmail.com>
2025-09-22 11:56:18 +0200

    faq finished, subscribe/footer section added

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -25,7 +25,7 @@ for (i = 0; i < acc.length; i++) {
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
         } else {
-            panel.style.maxHeight = panel.scrollHeight + "rem";
+            panel.style.maxHeight = panel.scrollHeight + "px";
         }
     })
 }
